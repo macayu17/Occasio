bd502d3be086a925f369d461871bec938e2025bd
macayu17 <anayush1406@gmail.com>
2026-01-31 18:21:36 +0530

    Favicon modified

--- frontend/src/components/CertificateDesigner.jsx
+++ frontend/src/components/CertificateDesigner.jsx
@@ -3,8 +3,6 @@ import { Document, Page, pdfjs } from 'react-pdf';
 import { Upload, Save, Type, Calendar, Trash2 } from 'lucide-react';
 import api from '../utils/api';
 import toast from 'react-hot-toast';
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-import 'react-pdf/dist/esm/Page/TextLayer.css';
 
 // Configure PDF worker
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
